09e5e607bd3679714fc1e26bfffee7fa19667837
Ben Cornelis & Garrett Olson <student@epicodus.com>
2015-06-01 16:46:37 -0700

    add task sorting function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var tasks = [];
+
 $(function() {
 
   var resetForm = function() {
@@ -10,23 +12,19 @@ $(function() {
     var description = $("input#description").val();
     var urgency     = $("input[name=urgency]:checked").val();
 
-    var task = {description: description, urgency: urgency}
+    var task = {description: description, urgency: urgency};
+    tasks.push(task);
 
-    $("#uncompleted-tasks").append("<li class='uncompleted-task " + assignUrgencyColor(task.urgency) + "'>"
-    + "<span class='icon checkmark text-success glyphicon glyphicon-ok'>" +
-    "</span><span class='icon delete-icon text-danger glyphicon glyphicon-remove'></span>" + task.description + "</li>");
-
-    $(".checkmark").last().click(function() {
-      $(".uncompleted-task").last().remove();
-      $("#completed-tasks").append("<li class='text-success'>" + task.description + "</li>");
-    });
-
-    $(".delete-icon").click(function() {
-      $(this).parent().remove();
-    });
+    appendTask(task);
 
     resetForm();
   });
+
+  $("#sort").click(function() {
+    $("#uncompleted-tasks").empty();
+    displayTasks(sortByUrgency(tasks));
+  });
+
 });
 
 var assignUrgencyColor = function(urgency) {
@@ -37,4 +35,45 @@ var assignUrgencyColor = function(urgency) {
   } else {
     return "text-danger";
   }
+}
+
+var sortByUrgency = function(taskArray) {
+  return taskArray.sort(function(task1, task2) {
+    if (task1.urgency < task2.urgency) {
+      return 1;
+    }
+    if (task1.urgency > task2.urgency) {
+      return -1;
+    }
+    return 0;
+  });
+};
+
+var displayTasks = function(taskArray) {
+  taskArray.forEach (function(task) {
+    appendTask(task);
+  });
+};
+
+var appendTask = function(task) {
+  $("#uncompleted-tasks").append("<li class='uncompleted-task " + assignUrgencyColor(task.urgency) + "'>"
+  + "<span class='icon checkmark text-success glyphicon glyphicon-ok'>" +
+  "</span><span class='icon delete-icon text-danger glyphicon glyphicon-remove'></span>" + task.description + "</li>");
+
+  $(".checkmark").last().click(function() {
+    $(this).parent().remove();
+    removeTask(task);
+    $("#completed-tasks").append("<li class='text-success'>" + task.description + "</li>");
+  });
+
+  $(".delete-icon").click(function() {
+    $(this).parent().remove();
+
+    removeTask(task);
+  });
+}
+
+var removeTask = function(task) {
+  var index = tasks.indexOf(task);
+  tasks.splice(index, 1);
 }
